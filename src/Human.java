b23d98c0d475b49074dac0cc1d45cf59d841f1cc
Kasper-Bahr <kasperbahr@wp.pl>
2023-01-20 21:40:24 +0100

    Zadanie 3,4

--- src/Human.java
+++ src/Human.java
@@ -1,10 +1,27 @@
+import java.util.Date;
+
 public class Human {
     Animal pet;
     String name;
+    private Double salary;
 
-    public Human(){
-        this.name = "kasper";
+    public void setSalary(Double salary) {
+        if (salary < 0){
+            System.out.println("wypłata nie może być ujemna");
+        }else {
+            System.out.println("nowe dane zostały wysłane do systemu księgowego");
+            System.out.println("konieczność odebrania aneksu do umowy od pani Hani z kadr");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty nie ma sensu ukrywać wynagrodzenia");
+        }
+        this.salary = salary;
     }
+    public Double getSalary() {
+        System.out.println("data dostępu do wynagrodzenia:" + new Date());
+        System.out.println("Wyoskosc wynagrodzenia: " + salary);
+        return salary;
+    }
+
+
 
     private Car car;
     public void setCar(Car car) {
@@ -13,6 +30,9 @@ public class Human {
     public Car getCar() {
         return car;
     }
+
+
+
     public String toString () {
         return "imie" + this.name;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Human human = new Human();
-        System.out.println(human);
-        Car car = new Car("Mustang", "Ford");
-        System.out.println(car);
-        human.setCar(car);
+        human.setSalary(5.000)
+        human.getSalary();
 
 
 
