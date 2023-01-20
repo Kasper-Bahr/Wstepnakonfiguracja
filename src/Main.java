f821088b470e75118ae3745791a8f7d1a3a4fc66
Kasper-Bahr <kasperbahr@wp.pl>
2023-01-20 23:01:35 +0100

    Zadanie 6

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import devices.Car;
+
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human();
-        human.setSalary(55000.0);
-        Car car = new Car("mustang", "ford");
-        car.setValue(15000.0);
-        human.setCar(car);
-        System.out.println(car);
+        Car car1 = new Car("mustang", "ford");
+        Car car2 = new Car("mustangg", "ford");
+        System.out.println(car1);
+        System.out.println(car2);
+        if (car1.equals(car2)){
+            System.out.println("car 1 jest równy car2");
+        }else {
+            System.out.println("car 1 nie jest równy car2");
+        }
+
+        }
     }
-}
--- src/devices/Car.java
+++ src/devices/Car.java
@@ -1,3 +1,5 @@
+package devices;
+
 public class Car {
    private String model;
    private String producer;
@@ -15,6 +17,15 @@ public class Car {
         this.model = model;
         this.producer = producer;
     }
+
+
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return model.equals(car.model) && producer.equals(car.producer);
+    }
+
     public String toString () {
         return "Model" + this.model + "Producent" + this.producer;
     }
